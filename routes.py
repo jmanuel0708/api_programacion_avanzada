2cd8cdb84868760625233aa293d9162395f1f2c5
Josefo Arraco <josefo.juarez@gmail.com>
2025-02-11 22:21:41 -0500

    Cambios realizados durante la clase del 11 de febrero del 2025

--- routes.py
+++ routes.py
@@ -4,28 +4,37 @@ from methods import crear_cuenta, iniciar_sesion, encontrar_todos_los_usuarios
 
 def cargar_rutas(app):
 # Este bloque de código es la base para todas rutas
-  @app.route('/')
-  def pagina():
+  @app.route('/<status>')
+  def pagina(status):
     lista_usuarios = encontrar_todos_los_usuarios()
 
-    for usuario in lista_usuarios:
-      print(f"""
-      Usuario: {usuario.name}
-      Correo: {usuario.email}
-      Password: {usuario.password}
-      """)
+#    for usuario in lista_usuarios:
+#      print(f"""
+#      Usuario: {usuario.name}
+#      Correo: {usuario.email}
+#      Password: {usuario.password}
+#      """)
 
+    
+    print(status)
     return render_template('index.html')
 
   # Esta es otra ruta
   @app.route('/login')
   def informacion_jose():
+
+    resultado = request.args.get('status')
+
+
     return render_template('login.html')
 
   # Esta es otra ruta
   @app.route('/signup')
   def datos():
-    return render_template('signup.html')
+
+    resultado = request.args.get('status')
+
+    return render_template('signup.html', estado=resultado)
 
   # Esta ruta va a manejar la información
   # Este método solo funcionará para el inicio de sesión
@@ -39,7 +48,10 @@ def cargar_rutas(app):
       Contraseña: {password}
   ''')
 
-    iniciar_sesion()
+    respuesta_login iniciar_sesion()
+
+    if respuesta_login['status'] == 'error':
+      return redirect(url_for('informacion_jose', status=respuesta_login['status']))
 
     return redirect(url_for('pagina'))
 
@@ -56,9 +68,17 @@ def cargar_rutas(app):
     passoword: {password}
   ''')
 
-    crear_cuenta(nombre, email, password)
+    respuesta_signup = crear_cuenta(nombre,email,password)
 
+    print(respuesta_signup)
+
+    if respuesta_signup['status'] == 'error':
+      return redirect(url_for('datos', status=respuesta_signup['status']))
 
 
     
-    return redirect(url_for('pagina'))
+    return redirect(url_for('pagina', status=respuesta_signup['status']))
+
+  @app.route('/error')
+  def pantalla_error():
+    return render_template('error.html')
